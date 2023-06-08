701f8784c06ea275bc5e753edb26b4801151b594
Dovineowuor <owuordove@gmail.com>
2023-06-08 20:16:02 +0300

    update environment variables

--- domatex_soln/settings.py
+++ domatex_soln/settings.py
@@ -9,18 +9,22 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load environment variables from.env file
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k+6u^gam36t6uj8o&u6xvkc)&79p1p=5nb9c6+cg0$*zdw6#kv'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'telehealth',
     'ehr',
-    'wallet',
+    'wallet' ,
     'solana',
     'bitcoin',
 ]
